b53261c635a330905af1b28c8d75d007dcd879c7
Paranjay Sharma <paranjas@andrew.cmu.edu>
2016-02-13 21:56:27 -0500

    Request Check Web Service

--- src/controller/customer/CustomerRequestCheckAction.java
+++ src/controller/customer/CustomerRequestCheckAction.java
@@ -10,75 +10,77 @@ import model.Model;
 
 import org.genericdao.RollbackException;
 import org.genericdao.Transaction;
-import org.mybeans.form.FormBeanException;
-import org.mybeans.form.FormBeanFactory;
+//import org.mybeans.form.FormBeanException;
+//import org.mybeans.form.FormBeanFactory;
+import com.google.gson.JsonObject;
 
+import util.Log;
 import util.Util;
 import controller.main.Action;
 import databean.CustomerBean;
-import formbean.RequestCheckForm;
+//import formbean.RequestCheckForm;
 
 public class CustomerRequestCheckAction extends Action {
 
 	private static final String FORMAT_STRING = "###,###,###,###,###,##0.00";
-	private static final String ACTION_NAME = "customer-request-check.do";
-	private static final String REQUES_CHECK_JSP = "customer-request-check.jsp";
-	private FormBeanFactory<RequestCheckForm> formBeanFactory;
+	//private static final String ACTION_NAME = "customer-request-check.do";
+	//private static final String REQUES_CHECK_JSP = "customer-request-check.jsp";
+	//private FormBeanFactory<RequestCheckForm> formBeanFactory;
 	private CustomerDAO customerDAO;
 	private Model model;
+	public static final String NAME = "requestCheck";
 
 	public CustomerRequestCheckAction(Model model) {
 		customerDAO = model.getCustomerDAO();
-		formBeanFactory = FormBeanFactory.getInstance(RequestCheckForm.class);
+		//formBeanFactory = FormBeanFactory.getInstance(RequestCheckForm.class);
 		this.model = model;
 	}
 
 	@Override
 	public String getName() {
-		return ACTION_NAME;
+		return NAME;
 	}
 
 	@Override
-	public String perform(HttpServletRequest request) {
+	public JsonObject perform(HttpServletRequest request) {
+		String amount = request.getParameter("cashValue");
+		Log.i("DC Action", "cashValue " + amount);
 		List<String> errors = new ArrayList<String>();
 		request.setAttribute("errors", errors);
 		try {
-			RequestCheckForm form = formBeanFactory.create(request);
-			CustomerBean customer = (CustomerBean) request.getSession().getAttribute(
-			    "customer");
+			//RequestCheckForm form;
+			//form = formBeanFactory.create(request);
+
+			CustomerBean customer = (CustomerBean) request.getSession().getAttribute("customer");
 			customer = customerDAO.read(customer.getId());
 			request.setAttribute("customer", customer);
 			double[] arr = model.getAmount(customer.getId());
 			double currentAmount = arr[0] / 100;
 			double validAmount = arr[2] / 100;
-			request.setAttribute("currentAmount",
-			    Util.formatNumber(currentAmount, FORMAT_STRING));
-			request.setAttribute("validAmount",
-			    Util.formatNumber(validAmount, FORMAT_STRING));
+			request.setAttribute("currentAmount", Util.formatNumber(currentAmount, FORMAT_STRING));
+			request.setAttribute("validAmount", Util.formatNumber(validAmount, FORMAT_STRING));
 
-			if (!form.isPresent()) {
-				return REQUES_CHECK_JSP;
-			}
-			
-			errors.addAll(form.getValidationErrors());
-			if (errors.size() != 0) {
-				return REQUES_CHECK_JSP;
-			}
-			model.commitRequestCheck(customer.getId(),
-			    ((long)(Double.parseDouble(form.getAmount()) * 100)));
-			request
-			    .setAttribute("message",
-			        "Thanks, we have accepted your request. Please wait until the Transition Day");
-			return "customer_success.jsp";
-		} catch (FormBeanException e) {
-			errors.add(e.toString());
-			return REQUES_CHECK_JSP;
-		} catch (RollbackException e) {
+			/*
+			 * if (!form.isPresent()) { return REQUES_CHECK_JSP; }
+			 * 
+			 * errors.addAll(form.getValidationErrors()); if (errors.size() !=
+			 * 0) { return REQUES_CHECK_JSP; }
+			 */
+			model.commitRequestCheck(customer.getId(), ((long) (Double.parseDouble(amount) * 100)));
+			JsonObject innerObject = new JsonObject();
+			innerObject.addProperty("message", "The withdrawal was successfully completed");
+
+			request.setAttribute("message", "The withdrawal was successfully completed");
+			return innerObject;
+		} /*catch (FormBeanException e) {
+			e.printStackTrace();
+			return null;
+		}*/ catch (RollbackException e) {
 			errors.add(e.getMessage());
-			return REQUES_CHECK_JSP;
+			return null;
 		} catch (NumberFormatException e) {
 			errors.add(e.toString());
-			return REQUES_CHECK_JSP;
+			return null;
 		} finally {
 			if (Transaction.isActive()) {
 				Transaction.rollback();
